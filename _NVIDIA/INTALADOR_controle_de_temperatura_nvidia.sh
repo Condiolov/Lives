fc10821b8d2995995c2d3898adcce4f476aa3a6b
Thiago Condé <thiagocondiolov@hotmail.com>
2024-12-28 14:44:30 -0300

    Agora ta funcionando depois do reiniciar pc

--- _NVIDIA/INTALADOR_controle_de_temperatura_nvidia.sh
+++ _NVIDIA/INTALADOR_controle_de_temperatura_nvidia.sh
@@ -4,6 +4,19 @@ sudo cp controle_de_temperatura_nvidia.service /etc/systemd/user/controle_de_tem
 
 #Recarregue o systemd para que ele reconheça o novo serviço:
 
+SUDOERS_FILE="/etc/sudoers"
+COMMAND_TO_ALLOW="/usr/bin/nvidia-settings"
+
+if ! sudo grep -q "$USER ALL=(ALL) NOPASSWD: $COMMAND_TO_ALLOW" "$SUDOERS_FILE"; then
+    echo "$USER ALL=(ALL) NOPASSWD: $COMMAND_TO_ALLOW" | sudo tee -a "$SUDOERS_FILE" >/dev/null
+    echo "Permissão adicionada para o comando $COMMAND_TO_ALLOW."
+else
+    echo "Permissão já existe."
+fi
+
+sudo usermod -aG video $USER
+
 systemctl --user daemon-reload
 systemctl --user enable controle_de_temperatura_nvidia.service
 systemctl --user start controle_de_temperatura_nvidia.service
+# systemctl --user status controle_de_temperatura_nvidia.service
--- _NVIDIA/controle_de_temperatura_nvidia.sh
+++ _NVIDIA/controle_de_temperatura_nvidia.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Ativar controle manual do cooler
-nvidia-settings -a '[gpu:0]/GPUFanControlState=1' >/dev/null 2>&1
-
 while true; do
   # Obter temperatura atual
   TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
 
   # Ajustar velocidade do cooler com base na temperatura
-  if [ "$TEMP" -gt 55 ]; then
+  if [ "$TEMP" -gt 50 ]; then
     nvidia-settings -a '[gpu:0]/GPUFanControlState=1' >/dev/null 2>&1
     nvidia-settings -a "[fan:1]/GPUTargetFanSpeed=100" >/dev/null 2>&1
     nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=100" >/dev/null 2>&1
-  elif [ "$TEMP" -ge 45 ] && [ "$TEMP" -le 55 ]; then
+  fi
+
+  if [ "$TEMP" -ge 40 ] && [ "$TEMP" -le 50 ]; then
     nvidia-settings -a '[gpu:0]/GPUFanControlState=1' >/dev/null 2>&1
     nvidia-settings -a "[fan:1]/GPUTargetFanSpeed=50" >/dev/null 2>&1
     nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=50" >/dev/null 2>&1
-  else
+  fi
+
+  if [ "$TEMP" -lt 40 ]; then
     nvidia-settings -a '[gpu:0]/GPUFanControlState=0' >/dev/null 2>&1
   fi
 
